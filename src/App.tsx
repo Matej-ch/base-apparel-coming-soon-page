2853f87a3a286217857cbf4170ead79e2eee3425
Matej <matejchalachan@gmail.com>
2023-01-13 18:33:23 +0100

    logic added

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,36 @@ import heroMobile from './assets/hero-mobile.jpg'
 import heroDesktop from './assets/hero-desktop.jpg'
 import logoSvg from './assets/logo.svg'
 import iconArrowSvg from './assets/icon-arrow.svg'
+import iconErrorSvg from './assets/icon-error.svg'
 import './App.scss'
+import React, {useState} from "react";
 
 function App() {
+
+    const [errorMsg, setErrorMsg] = useState<string>('');
+    const mailformat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const formData = new FormData(form);
+
+        const emailInputValue = formData.get('email');
+
+        if (!emailInputValue) {
+            setErrorMsg('Email field is empty');
+            return;
+        }
+
+        if (!emailInputValue.match(mailformat)) {
+            setErrorMsg('Please provide a valid email');
+            return;
+        }
+
+        setErrorMsg('');
+        return;
+    }
 
     return (
         <main className="App">
@@ -29,10 +56,12 @@ function App() {
                     Add your email below to stay up-to-date with announcements an our launch deals.
                 </p>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className={'input-wrapper'}>
-                        <input type="text" placeholder={'Email Address'}/>
+                        <input type="text" name={'email'} placeholder={'Email Address'}/>
                         <button type='submit'><img src={iconArrowSvg} alt=""/></button>
+                        {errorMsg.length ? <img className={'error-icon'} src={iconErrorSvg} alt=""/> : null}
+                        {errorMsg.length ? <span className={'error-msg'}>{errorMsg}</span> : null}
                     </div>
 
                 </form>
